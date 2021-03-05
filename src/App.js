abb8db9adea07bbc34b2675566d810d88cda9564
Flooweron <63611265+Flooweron@users.noreply.github.com>
2021-03-06 02:15:51 +0300

    big

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ class App extends Component {
     row: null,
     currentPage: 0,
   }
-  async fetchData(url) {
-    const response = await fetch(`https://www.filltext.com/?rows=250&id={number|250}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
+  async fetchData(bigUrl) {
+    const response = await fetch(bigUrl)
     const data = await response.json()
    
     this.setState({
