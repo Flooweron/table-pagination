d9ef229791dea7883ee72f80352e0614592063eb
Flooweron <63611265+Flooweron@users.noreply.github.com>
2021-03-06 01:50:53 +0300

    s

--- src/ModeSelector/ModeSelector.js
+++ src/ModeSelector/ModeSelector.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default props =>{
-    const smallUrl = `http://www.filltext.com/?rows=50&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
-    const bigUrl = `http://www.filltext.com/?rows=250&id={number|250}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+    const smallUrl = `https://www.filltext.com/?rows=50&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+    const bigUrl = `https://www.filltext.com/?rows=250&id={number|250}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
     return (
         <div style={{display:'flex', justifyContent:'center', padding: '50px 0'}}>
             <button onClick={()=>props.onSelect(smallUrl)} className="btn btn-success">50 человек </button>
