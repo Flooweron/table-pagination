7056018aec2721a984257effa962c1d69be4594f
Flooweron <63611265+Flooweron@users.noreply.github.com>
2021-03-06 17:31:25 +0300

    a

--- src/ModeSelector/ModeSelector.js
+++ src/ModeSelector/ModeSelector.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default props =>{
-    const bigUrl = `http://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+    const bigUrl = `https://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
     return (
         <div style={{display:'flex', justifyContent:'center', padding: '50px 0'}}>
             <button onClick={()=>props.onSelect(bigUrl)} className="btn btn-danger">250 элементов</button>
