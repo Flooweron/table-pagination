78646fa86d80199dbfd2a676876136c2f2839f28
Flooweron <63611265+Flooweron@users.noreply.github.com>
2021-03-08 19:22:27 +0300

    daas

--- src/ModeSelector/ModeSelector.js
+++ src/ModeSelector/ModeSelector.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default props =>{
-    const bigUrl = `http://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+    const url = `http://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
     return (
         <div style={{display:'flex', justifyContent:'center', padding: '50px 0'}}>
-            <button onClick={()=>props.onSelect(bigUrl)} className="btn btn-danger">250 элементов</button>
+            <button onClick={()=>props.onSelect(url)} className="btn btn-danger">250 элементов</button>
         </div>
     )
 }
